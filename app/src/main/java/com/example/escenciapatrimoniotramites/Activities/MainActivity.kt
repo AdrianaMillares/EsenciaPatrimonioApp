2e41bd7298c4f3816aaa816ec367dcb5f6bd7d8c
Pau <A01701490@itesm.mx>
2021-09-25 00:19:55 -0500

    Fix bottom navigation not changing

--- app/src/main/java/com/example/escenciapatrimoniotramites/Activities/MainActivity.kt
+++ app/src/main/java/com/example/escenciapatrimoniotramites/Activities/MainActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.escenciapatrimoniotramites.Fragmentos.HomeFragment
 import com.example.escenciapatrimoniotramites.Fragmentos.ProfileFragment
 import com.example.escenciapatrimoniotramites.R
@@ -17,55 +19,53 @@ class MainActivity : AppCompatActivity() {
     val TAG : String = "MainActivity"
 
     lateinit var btnLogOut : Button
-    private lateinit var bottomNav : BottomNavigationView
+    lateinit var bottomNav : BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // define your fragments here
-        //val home: Fragment = FirstFragment()
-        //val profile: Fragment = ProfileFragment()
-        //val fragment3: Fragment = ThirdFragment()
+        // Se definen los fragmentos de las pestañas
+        val homeFragment = HomeFragment()
+        val profileFragment = ProfileFragment()
+
 
         // Se encuentran los componentes de la pantalla
         //btnLogOut = findViewById(R.id.btnLogOut)
-        bottomNav = findViewById(R.id.bottom_navigation)
+        bottomNav = findViewById(R.id.bottomNavigation)
 
         /*btnLogOut.setOnClickListener {
             ParseUser.logOut()
             goLoginActivity()
         }*/
 
+        openFragment(homeFragment)
         bottomNav.setOnItemSelectedListener { item ->
-            //var fragment : Fragment
             when (item.itemId) {
                 R.id.action_home ->{
-                    val fragment = HomeFragment.newInstance()
-                    openFragment(fragment)
+                    openFragment(homeFragment)
                     true
-                }         // do something here
+                }
                 R.id.action_search ->{
-                    val fragment = ProfileFragment.newInstance()
-                    openFragment(fragment)
+                    openFragment(homeFragment)
                     true
-                }       // do something here
+                }
                 R.id.action_contacts -> {
-                    val fragment = ProfileFragment.newInstance()
-                    openFragment(fragment)
+                    openFragment(homeFragment)
                     true
-                }      // do something here
+                }
                 R.id.action_profile ->{
-                    val fragment = ProfileFragment.newInstance()
-                    openFragment(fragment)
+                    openFragment(profileFragment)
                     true
-                }    // do something here
+                }
                 R.id.action_donation ->{
-                    val fragment = ProfileFragment.newInstance()
-                    openFragment(fragment)
+                    openFragment(homeFragment)
                     true
-                }         // do something here
-                else -> true
+                }
+                else -> {
+                    openFragment(homeFragment)
+                    true
+                }
             }
         }
     }
@@ -77,10 +77,9 @@ class MainActivity : AppCompatActivity() {
         finishAffinity() // Cierra todas las ventanas anteriores
     }
 
-    private fun openFragment(fragment: Fragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.main_container, fragment)
-        transaction.addToBackStack(null)
-        transaction.commit()
+    private fun openFragment(fragmnt: Fragment) {
+        val frag = supportFragmentManager.beginTransaction()
+        frag.replace(R.id.flContainer, fragmnt)
+        frag.commit()
     }
 }
