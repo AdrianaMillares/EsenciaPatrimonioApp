d379893890569a5f04d063fc4f703ed2d319a905
AdriMillares <a01705674@itesm.mx>
2021-09-28 10:41:31 -0500

    Search

--- app/src/main/java/com/example/escenciapatrimoniotramites/Fragmentos/SearchFragment.kt
+++ app/src/main/java/com/example/escenciapatrimoniotramites/Fragmentos/SearchFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.escenciapatrimoniotramites.R
 
 
@@ -18,4 +23,46 @@ class SearchFragment : Fragment() {
     companion object {
         fun newInstance(): ProfileFragment = ProfileFragment()
     }
+    // TODO ARREGLAR SEARCH
+/*
+    lateinit var searchView: SearchView
+    lateinit var listView: ListView
+    lateinit var list: ArrayList<String>
+    lateinit var adapter: ArrayAdapter<*>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        searchView = View.findViewById(R.id.searchView)
+        listView = View.findViewById(R.id.listView)
+        list = ArrayList()
+        list.add("Apple")
+        list.add("Banana")
+        list.add("Pineapple")
+        list.add("Orange")
+        list.add("Mango")
+        list.add("Grapes")
+        list.add("Lemon")
+        list.add("Melon")
+        list.add("Watermelon")
+        list.add("Papaya")
+        adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list)
+        listView.adapter = adapter
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                if (list.contains(query)) {
+                    adapter.filter.filter(query)
+                } else {
+                    Toast.makeText(this@SearchFragment, "No Match found", Toast.LENGTH_LONG).show()
+                }
+                return false
+            }
+            override fun onQueryTextChange(newText: String): Boolean {
+                adapter.filter.filter(newText)
+                return false
+            }
+        })
+    }
+ */
+
+
 }
