ca35a882649903e3b4ab3adfe75a0e1321c17bee
Cutberto <46075159+Cutberto@users.noreply.github.com>
2021-09-29 12:42:58 -0500

    primer test de comentarios

--- app/src/main/java/com/example/escenciapatrimoniotramites/Activities/InformationActivity.kt
+++ app/src/main/java/com/example/escenciapatrimoniotramites/Activities/InformationActivity.kt
@@ -2,11 +2,55 @@ package com.example.escenciapatrimoniotramites.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.escenciapatrimoniotramites.R
+import com.parse.ParseObject
+import com.parse.ParseUser
 
 class InformationActivity : AppCompatActivity() {
+
+    val TAG = "InformtionActivity"
+     lateinit var etComment: EditText
+    lateinit var btnPublish: Button
+    lateinit var etTramiteId: TextView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_information)
+        btnPublish = findViewById(R.id.btnComentar)
+        etComment = findViewById(R.id.etComentario)
+
+        btnPublish.setOnClickListener{
+            Log.i(TAG, "onClick login button")
+            var comentario = etComment.text.toString()
+            var usuario = ParseUser.getCurrentUser().toString()
+            var idTramite ="1" // todo: definir como obtener el idtramitec
+            Log.i(TAG, "username: $usuario comentario: $comentario")
+            saveNewComment(comentario, usuario, idTramite)
+        }
+
     }
+
+
+    fun saveNewComment(comentario:String, usuario:String, idTramite:String ) {
+
+        val comentar = ParseObject("Comentar")
+        comentar.put("idComentario", "id");
+        comentar.put("idTramite", idTramite);
+        comentar.put("idUsario", usuario)
+        comentar.put("comentario", comentario);
+
+        comentar.saveInBackground{error ->
+            if (error == null){
+                Log.d("Salida","Guardado Correctamente")
+            }
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/escenciapatrimoniotramites/Modelos/Comentar.kt
+++ app/src/main/java/com/example/escenciapatrimoniotramites/Modelos/Comentar.kt
@@ -25,4 +25,32 @@ class Comentar : ParseObject() {
     var usuario: ParseUser?
         get() = getParseUser(LLAVE_USUARIO)
         set(usuario) = put(LLAVE_USUARIO, usuario!!)
+
+
 }
+
+/*
+*
+        with(user) {
+            username = nombreusuario
+            setPassword(password)
+            email = mail
+
+
+            signUpInBackground { e ->
+                if (e == null) {
+                    Log.i(TAG, "Al parecer todo salio bien, usuario: $username, correo: $email")
+                    goLoginActivity()
+                    Toast.makeText(applicationContext, "Registro exitoso", Toast.LENGTH_SHORT).show()
+
+                } else {
+                    Log.i(TAG, "No se que pudo salio mal :(")
+                    Toast.makeText(applicationContext, "Algo falló, vuelve a intentarlo", Toast.LENGTH_SHORT).show()
+
+                    // Sign up didn't succeed. Look at the ParseException
+                    // to figure out what went wrong
+                }
+            }
+        }
+*
+* */
