d7fcf7761e00edc922957059e7c6992042c733ae
Pau <A01701490@itesm.mx>
2021-10-04 23:40:41 -0500

    Add logged user's info in Profile

--- app/src/main/java/com/example/escenciapatrimoniotramites/Fragmentos/HomeFragment.kt
+++ app/src/main/java/com/example/escenciapatrimoniotramites/Fragmentos/HomeFragment.kt
@@ -1,13 +1,22 @@
 package com.example.escenciapatrimoniotramites.Fragmentos
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.app.ActivityCompat.finishAffinity
+import com.example.escenciapatrimoniotramites.Activities.LoginActivity
 import com.example.escenciapatrimoniotramites.R
+import com.parse.ParseUser
 
 class HomeFragment : Fragment() {
+
+    val TAG : String = "HomeFragment"
+    lateinit var btnLogOut : Button
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -16,5 +25,22 @@ class HomeFragment : Fragment() {
 
     companion object {
         fun newInstance(): HomeFragment = HomeFragment()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnLogOut = view.findViewById(R.id.btnLogOut)
+
+        btnLogOut.setOnClickListener {
+            ParseUser.logOut()
+            goLoginActivity()
+        }
+    }
+
+    private fun goLoginActivity() {
+        Log.i(TAG, "Entered goMainActivity")
+        val i = Intent(context, LoginActivity::class.java)
+        startActivity(i)
+        activity?.finishAffinity() // Cierra todas las ventanas anteriores
     }
 }
--- app/src/main/java/com/example/escenciapatrimoniotramites/Fragmentos/ProfileFragment.kt
+++ app/src/main/java/com/example/escenciapatrimoniotramites/Fragmentos/ProfileFragment.kt
@@ -1,14 +1,30 @@
 package com.example.escenciapatrimoniotramites.Fragmentos
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.example.escenciapatrimoniotramites.R
+import com.parse.ParseUser
 
 
 class ProfileFragment : Fragment() {
+
+    private val TAG : String = "ProfileActivity"
+    private val LLAVE_PROFILE_PICTURE = "profilePicture"
+    protected val currentUser : ParseUser = ParseUser.getCurrentUser()
+
+    lateinit var ivProfile : ImageView
+    lateinit var tvUserProfile : TextView
+    lateinit var tvEmailProfile : TextView
+    lateinit var btnCambiarPass : Button
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -17,5 +33,27 @@ class ProfileFragment : Fragment() {
 
     companion object {
         fun newInstance(): ProfileFragment = ProfileFragment()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Encuentra los componentes de la vista
+        ivProfile = view.findViewById(R.id.ivProfile)
+        tvUserProfile = view.findViewById(R.id.tvUserProfile)
+        tvEmailProfile = view.findViewById(R.id.tvEmailProfile)
+        btnCambiarPass = view.findViewById(R.id.btnCambiarPass)
+
+        // Establece el texto correspondiente al usuario loggeado
+        Log.d(TAG, "username: " + currentUser.username + "email: " + currentUser.email)
+        tvUserProfile.setText(currentUser.username)
+        tvEmailProfile.setText(currentUser.email)
+        Log.i(TAG, "url: " + currentUser.getParseFile(LLAVE_PROFILE_PICTURE)?.url)
+        Glide.with(this).load(currentUser.getParseFile(LLAVE_PROFILE_PICTURE)?.url).circleCrop().into(ivProfile)
+
+        // Funcionalidad de cambiar contrasena
+        btnCambiarPass.setOnClickListener {
+            Log.i(TAG, "Entered btnCambiarPass")
+        }
     }
 }
