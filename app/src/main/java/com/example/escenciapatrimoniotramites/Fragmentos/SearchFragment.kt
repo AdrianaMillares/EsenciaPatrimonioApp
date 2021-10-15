cc22df47ce0fa3429746f30d904c89b56c2bafec
AdriMillares <a01705674@itesm.mx>
2021-10-14 22:33:45 -0500

    SearchFragment comentado con JavaDoc

--- app/src/main/java/com/example/escenciapatrimoniotramites/Fragmentos/SearchFragment.kt
+++ app/src/main/java/com/example/escenciapatrimoniotramites/Fragmentos/SearchFragment.kt
@@ -15,27 +15,41 @@ import com.example.escenciapatrimoniotramites.Modelos.Tramite
 import com.example.escenciapatrimoniotramites.R
 import com.parse.ParseQuery
 
-
+/**
+ * Obtiene la información de [Tramite]
+ * Implementa la busqueda de [Tramite]
+ * Despliega en pantalla los resultados en forma de lista
+ * @param searchView Elemento de la interfaz que permite realizar la busqueda
+ * @param listView Elemento de la interfaz donde se desplegara la información
+ * @param list Lista de strings donde se insertan los nombres de [Tramite] para evitar duplicidad
+ * @param adapter Permite enviar información a la interfaz
+ */
 class SearchFragment : Fragment() {
-
-    lateinit var searchView: SearchView
-    lateinit var listView: ListView
+    private lateinit var searchView: SearchView
+    private lateinit var listView: ListView
     var list: ArrayList<String> = ArrayList()
     lateinit var adapter: ArrayAdapter<*>
 
+    /**
+     * Se ejecuta al iniciar la vista
+     * @param vista Contiene la interfaz de [SearchFragment]
+     * @return la vista de la interfaz
+     */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
+    /**
+     * Se ejecuta al crear la vista
+     * Obtiene y despliega el nombre de los tramites
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         searchView = view.findViewById(R.id.searchView)
         listView = view.findViewById(R.id.listView)
 
-
         val query: ParseQuery<Tramite> = ParseQuery.getQuery(Tramite::class.java)
-        // Execute the find asynchronously
         query.findInBackground { itemList, e ->
             if (e == null) {
                 for (tramite in itemList) {
@@ -49,22 +63,19 @@ class SearchFragment : Fragment() {
             } else {
                 Log.d("SearchFragment", "Error: " )
             }
-
         }
 
+        // Despliega los datos en la interfaz
         val appContext = requireContext().applicationContext
         adapter = ArrayAdapter<String>(appContext, R.layout.list_item_theme, list)
         listView.adapter = adapter
-        //listView.onItemClickListener = onItemClick()
-
-        listView.setOnItemClickListener(OnItemClickListener { parent, view, position, id ->
+        listView.onItemClickListener = OnItemClickListener { parent, view, position, id ->
             val newActivity = Intent(context, InformationActivity::class.java)
             newActivity.putExtra("nombreTramite", listView.getItemAtPosition(position).toString())
             ( context as MainActivity?)!!.startActivity(newActivity)
+        }
 
-
-        })
-
+        // Se filtran los elementos segun los caracteres que ingrese el usuario
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
                 if (list.contains(query)) {
@@ -79,10 +90,6 @@ class SearchFragment : Fragment() {
                 return false
             }
         })
-    }
-
-    companion object {
-        fun newInstance(): ProfileFragment = ProfileFragment()
     }
 }
 
